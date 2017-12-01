7be59e371ca37d4a9dc0d306bd530bf85a6e3e66
Kang Da Sol <dskang070@naver.com>
2017-12-01 16:25:56 +0900

    test

--- tts/tts_complete.py
+++ tts/tts_complete.py
@@ -3,4 +3,4 @@ import sys
 import os
 
 # 2017.11.10 complete
-os.system("curl -d 'speaker=jinho&speed=0&text=$1' 'https://openapi.naver.com/v1/voice/tts.bin' -H 'Content-Type: application/x-www-form-urlencoded' -H 'X-Naver-Client-Id: wY8qYOdN9FzbBBrgtlF3' -H 'X-Naver-Client-Secret: _dblCskdHA' > tts.wav")
+os.system("curl -d 'speaker=jinho&speed=0&text="+str(sys.argv[1])+"' 'https://openapi.naver.com/v1/voice/tts.bin' -H 'Content-Type: application/x-www-form-urlencoded' -H 'X-Naver-Client-Id: wY8qYOdN9FzbBBrgtlF3' -H 'X-Naver-Client-Secret: _dblCskdHA' > tts.wav")
