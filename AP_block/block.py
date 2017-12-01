443355a3ab28b1de42247e1f3009fae88605e6f0
Kang Da Sol <dskang070@naver.com>
2017-12-01 16:37:23 +0900

    fix bug

--- AP_block/block.py
+++ AP_block/block.py
@@ -1,10 +1,16 @@
 # coding=utf8
 
-# 
+import os
+import sys
 class Block:
 	def block_mac(self):
 		print("block mac")
 
 if __name__=="__main__":
-	block=Block()
-	block.block_mac()	
+	try:
+		if sys.argv[1]=="1":
+			block=Block()
+			block.block_mac()	
+	except:
+		#os.system("omxplayer please_admin.mp3")
+		sys.exit(1)
